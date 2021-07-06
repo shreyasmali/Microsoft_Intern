ba90eb05e4247838a446f5098b0eecc65379609d
Shreyas Mali <52993608+shreyasmali@users.noreply.github.com>
2021-07-06 18:22:15 +0530

    Add files via upload
    Working zip file and StartupScript.sh along with User provided files such as (CommandsToExecute.sh, temp.sh, temp2.sh)
    Code capable of copying any number of user provided *.sh scripts and running them as per commands provided in CommandsToExecute.sh file

---
+++ CommandsToExecute.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#cd /usr/local/AzureRecovery/user_files
+bash /usr/local/AzureRecovery/user_files/temp.sh
+bash temp2.sh
---
+++ temp2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Similar file with temp2.sh as a name"
+
+echo "printing second line"
+ls -latr
+touch /usr/local/AzureRecovery/user_files/file_using_temp2.txt
